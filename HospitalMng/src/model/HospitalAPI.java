58292fd9867a4c39ae9e0753c3aa7b08b264c375
Dulmini Kalupahana <dulhesh7@gmail.com>
2020-05-04 23:47:07 +0530

    Setup the Dc-Bus

--- HospitalMng/src/model/HospitalAPI.java
+++ HospitalMng/src/model/HospitalAPI.java
@@ -1,6 +1,9 @@
 package model;
 
-import java.io.IOException;
+import java.io.IOException; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner; 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +23,7 @@ public class HospitalAPI extends HttpServlet {
     public HospitalAPI() {
         super();
         // TODO Auto-generated constructor stub
+        
     }
 
 	/**
@@ -35,7 +39,15 @@ public class HospitalAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		Hospital hosObj = new Hospital();
+		
+		String output = hosObj.insertHosDetails(request.getParameter("hosName"),      
+				request.getParameter("hosAddress"),     
+				request.getParameter("hosContactno"),       
+				request.getParameter("hosEmail")); 
+		 
+		 response.getWriter().write(output);
 	}
 
 	/**
@@ -43,6 +55,16 @@ public class HospitalAPI extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		Hospital hosObj = new Hospital();
+		 
+		 String output = hosObj.updateHosDetails(paras.get("hidHospIDSave").toString(),     
+				 paras.get("hosName").toString(),     
+				 paras.get("hosAddress").toString(),        
+				 paras.get("hosContactno").toString(),        
+				 paras.get("hosEmail").toString()); 
+		 
+		 response.getWriter().write(output);
 	}
 
 	/**
@@ -50,6 +72,39 @@ public class HospitalAPI extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		Hospital hosObj = new Hospital();
+		 
+		 String output = hosObj.deleteHosDetails(paras.get("hosID").toString()); 
+		 
+		 response.getWriter().write(output);
+	}
+	
+	
+	private static Map getParasMap(HttpServletRequest request) 
+	{  
+		Map<String, String> map = new HashMap<String, String>();  
+		try  
+		{   
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");   
+			String queryString = scanner.hasNext() ?          
+					scanner.useDelimiter("\\A").next() : "";   
+					scanner.close(); 
+	 
+					String[] params = queryString.split("&");   
+					for (String param : params)   
+					{ 
+		  
+						String[] p = param.split("=");    
+						map.put(p[0], p[1]);   
+					}  
+		}  
+		catch (Exception e)  
+		{  
+			
+		}  
+		return map; 
+		
 	}
 
 }
